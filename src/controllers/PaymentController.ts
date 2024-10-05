14cdb513800b2c793f671b8be41c137343f11fb7
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-10-04 21:29:28 -0400

    feat: :sparkles: add argument bonification in schema prisma

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -9,6 +9,7 @@ interface PaymentProps {
   store: string;
   datePayment: string;
   overTime?: string | null;
+  bonification?: string | null;
   advanceMoney?: string | null;
   cardLoan?: string | null;
   discounts: string | null;
@@ -26,6 +27,7 @@ class PaymentController {
         salaryTotal = "",
         datePayment = "",
         overTime = "",
+        bonification = "",
         advanceMoney = "",
         cardLoan = "",
         discounts = "",
@@ -39,6 +41,7 @@ class PaymentController {
           salaryTotal: salaryTotal,
           datePayment: datePayment,
           overTime: overTime,
+          bonification: bonification,
           advanceMoney: advanceMoney,
           cardLoan: cardLoan,
           discounts: discounts,
@@ -61,6 +64,21 @@ class PaymentController {
     } catch (error) {
       console.log("Erro ao buscar os pagamentos /payments", error);
       res.status(500).json({ error: "Erro ao buscar os pagamentos" });
+    }
+  };
+
+  destroy = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const payment = await Prisma.payment.delete({
+        where: { id: id },
+      });
+      res
+        .status(200)
+        .json({ message: "Pagamento excluido com sucesso!", payment });
+    } catch (error) {
+      console.log("Erro ao excluir o pagamento /payments/:id", error);
+      res.status(500).json({ error: "Erro ao excluir o pagamento" });
     }
   };
 }
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -5,5 +5,6 @@ const route = Router();
 
 route.post("/payments", PaymentController.store);
 route.get("/payments", PaymentController.index);
+route.delete("/payments/:id", PaymentController.destroy);
 
 export default route;
